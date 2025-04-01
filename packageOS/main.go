56b90d5633244803921df7a6e6d91c299ce36c5e
Tomas Conesa <totoooc@gmail.com>
2025-04-01 17:11:40 +0200

    OS

---
+++ packageOS/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("main.go")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1) // Finaliza el programa
+	}
+	fmt.Println(file)
+	v, _ := file.Stat()
+	fmt.Println(v.Name(), v.Size())
+
+	data := make([]byte, 1024)
+	//fmt.Println(data)
+
+	c, err := file.Read(data)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(data[:c]), c)                 // Con string podemos leerlo, sino se muestra la información en bytes
+	fmt.Printf("read: %d bytes: %q \n", c, data[:c]) // Otra manera de leerlo
+
+	// Obtener el valor de una variable de entorno
+	fmt.Println(os.Getenv("USERNAME"))
+	os.Setenv("MI_ENV", "my value")
+	fmt.Println(os.Getenv("MI_ENV"))
+
+	dir, _ := os.Getwd()
+	fmt.Println(dir)
+
+	// Para saber si una variable de entorno existe
+	os.Setenv("ENV_EXISTS", "")
+	env, ok := os.LookupEnv("ENV_EXISTS")
+	fmt.Println(env, ok)
+}
